2571cf4ed63ceb68d710a3e55a4ba669023f7b75
Thvithran <73452153+thvithran@users.noreply.github.com>
2022-12-01 10:39:54 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+// This file is gonna create an object and its going to explore the particular object
+
+const constants = {
+    openWeatherMap : {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "bb39adadef0565da6bf19afb8337b2e1"
+    }
+}
+
+module.exports = constants;
